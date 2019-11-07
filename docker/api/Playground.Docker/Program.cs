74dd8d25ef56bd7b609a59a80a64eeeb19222565
Matthias Hillermeier <m.hillermeier@adestis.de>
2019-11-07 22:00:04 +0100

    goofing around

--- docker/api/Playground.Docker/Program.cs
+++ docker/api/Playground.Docker/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Docker.DotNet;
 using Docker.DotNet.Models;
+using ICSharpCode.SharpZipLib.GZip;
+using ICSharpCode.SharpZipLib.Tar;
 using System;
 using System.IO;
 using System.Text;
@@ -13,26 +15,83 @@ namespace Playground.Docker
         {
             var client = new DockerClientConfiguration(new Uri("http://host.docker.internal:2375"))
                 .CreateClient();
-            
-            var containers = await client.Containers.ListContainersAsync(new ContainersListParameters(){Limit = 10});
-            foreach (var container in containers)
-            {
-                Console.WriteLine(container.ID);
-            }
 
             var sb = new StringBuilder();
 
             sb.AppendLine("FROM mcr.microsoft.com/dotnet/core/runtime:3.0-buster-slim");
-            sb.AppendLine("ARG foo");
             sb.AppendLine("COPY . .");
-            sb.AppendLine("RUN dotnet --version");
+            sb.AppendLine("ENTRYPOINT [\"dotnet\", \"Playground.Docker.Hello.dll\"]");
 
-            var files = Directory.GetFiles(Directory.GetCurrentDirectory());
+            var contentsDir = Directory.CreateDirectory("contents");
 
-            using var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(sb.ToString()));
+            var dockerfilePath = Path.Combine("contents", "Dockerfile");
 
-            var image = await client.Images.BuildImageFromDockerfileAsync(memoryStream, new ImageBuildParameters
+            File.WriteAllText(dockerfilePath, sb.ToString());
+
+            if (!File.Exists("contents/Playground.Docker.Hello.dll"))
             {
+                File.Copy("Playground.Docker.Hello.dll", Path.Combine("contents", "Playground.Docker.Hello.dll"));
+            }
+
+            var filesInDirectory = contentsDir.GetFiles();
+            var tarArchiveName = @"contents.tar.gz";
+
+            using var targetStream = new GZipOutputStream(File.Create(tarArchiveName));
+            {
+                using var tarArchive = TarArchive.CreateOutputTarArchive(targetStream);
+                {
+                    foreach (var fileToBeTarred in filesInDirectory)
+                    {
+                        var entry = TarEntry.CreateEntryFromFile(fileToBeTarred.FullName);
+                        entry.Name = fileToBeTarred.Name;
+                        tarArchive.WriteEntry(entry, true);
+                    }
+                }
+            }
+
+            var extractedDir = Directory.CreateDirectory("extracted");
+
+            using (var sourceStream = new GZipInputStream(File.OpenRead(tarArchiveName)))
+            {
+                using (var t = TarArchive.CreateInputTarArchive(sourceStream, TarBuffer.DefaultBlockFactor))
+                {
+                    t.ExtractContents(extractedDir.FullName);
+                }
+            }
+
+            var files = extractedDir.GetFiles();
+
+            using var contentsStream = File.OpenRead(tarArchiveName);
+
+            var image = await client.Images.BuildImageFromDockerfileAsync(contentsStream, new ImageBuildParameters
+            {
+            });
+
+            var read = -1;
+
+            using var imageStream = File.OpenWrite("image.tar.gz");
+            {
+                do
+                {
+                    var b = new byte[1024];
+
+                    read = image.Read(b, 0, 1024);
+
+                    imageStream.Write(b, 0, b.Length);
+                } while (read > 0);
+                imageStream.Close();
+            }
+
+            using var imageReadStream = File.OpenRead("image.tar.gz");
+            {
+                await client.Images.CreateImageAsync(new ImagesCreateParameters
+                {
+                }, imageReadStream, null, null);
+            }
+
+            var response = await client.Containers.CreateContainerAsync(new CreateContainerParameters
+            {
+                
             });
         }
     }
